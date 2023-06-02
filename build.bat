596ba965baac02787304a08fb8e27cd6475106b5
Cutleast <cutleast@gmail.com>
2023-06-02 14:54:02 +0200

    Improved building scripts

--- build.bat
+++ build.bat
@@ -1,11 +1,1 @@
-@echo off
-nuitka ^
---msvc="latest" ^
---standalone ^
---disable-console ^
---include-data-dir=".\src\assets=.\assets" ^
---enable-plugin=pyside6 ^
---nofollow-import-to=tkinter ^
---windows-icon-from-ico=".\src\assets\icon.ico" ^
---output-filename="DRIP.exe" ^
-".\src\main.py"
+python build_with_fomod.py
